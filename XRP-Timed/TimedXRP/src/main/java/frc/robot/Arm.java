dab2ad7e60615e27b2c2c276143886f37cdc12dc
Enzo-Coutinho <enzo@stemos.com.br>
2025-01-23 14:03:13 -0300

    getter and setter

--- XRP-Timed/TimedXRP/src/main/java/frc/robot/Arm.java
+++ XRP-Timed/TimedXRP/src/main/java/frc/robot/Arm.java
@@ -6,11 +6,19 @@ public class Arm {
     
     XRPServo servo;
 
+    private double angle;
+
     public Arm(int channel) {
         servo = new XRPServo(channel);
+        angle = 0;
     }
 
     public void setAngle(double angle) {
+        this.angle = angle;
         servo.setAngle(angle);
+    }
+
+    public double getAngle() {
+        return angle;
     }
 }
